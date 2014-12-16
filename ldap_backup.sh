3d0c83a9f23ad0531fdc8c22acce628a110ce96a
Michael Schaarschmidt <michael@schaaryworks.com>
2014-12-16 16:41:58 +0100

    fix type errors

--- ldap_backup.sh
+++ ldap_backup.sh
@@ -4,7 +4,7 @@ DUMPFILE=dump-`date +%F-%H-%M-%S`
 EMAILADDRESS=''
 
 
-function fehler() # {{{
+function fehler() 
 {
   local rc=$1
   /usr/bin/logger -is -p ldap.err -t ldap_backup $*
@@ -14,7 +14,7 @@ function fehler() # {{{
   ) | /usr/bin/mail -s "Fehler bei LDAP-Backup!!!" ${EMAILADDRESS} 2>/dev/null
 
   exit $rc
-} # }}}
+} 
 
 ME=`ps -o user= -p $$ | awk '{print $1}'`
 
@@ -36,46 +36,45 @@ if [ ! -d '/archive/backup' ]; then
   /bin/mkdir /archive/backup
 fi
 
-
 /usr/bin/logger -is -p local0.info -t ldap_backup LDAP-Backup gestartet
 
 cd /archive/backup
-if test $? -ne 0 # {{{
+if test $? -ne 0 
 then
   fehler 1 "kann nicht nach /archive/backup wechseln"
-fi # }}}
+fi 
 
 #/etc/init.d/ldap stop 
 /usr/sbin/service slapd stop
-if test $? -ne 0 # {{{
+if test $? -ne 0 
 then
   fehler 2 "kann den LDAP-Server nicht stoppen"
-fi # }}}
+fi 
 
 /usr/sbin/slapcat -l ${DUMPFILE} 
-if test $? -ne 0 # {{{
+if test $? -ne 0 
 then
   fehler 3 "kann den Dump nicht ${DUMPFILE} schreiben"
-fi # }}}
+fi 
 
 #/etc/init.d/ldap start
 /usr/sbin/service slapd start
-if test $? -ne 0 # {{{
+if test $? -ne 0 
 then
   fehler 4 "kann den LDAP-Server nicht wieder starten"
-fi # }}}
+fi 
 
 /bin/bzip2 ${DUMPFILE}
-if test $? -ne 0 # {{{
+if test $? -ne 0 
 then
   fehler 5 "kann den Dump nicht packen"
-fi # }}}
+fi 
 
 /bin/chmod 600 /archive/backup/*
-if test $? -ne 0 # {{{
+if test $? -ne 0 
 then
   fehler 6 "kann die Zugriffsrechte in /data/backup nicht auf 600 setzen"
-fi # }}}
+fi 
 
 echo Alles bestens | /usr/bin/mail -s "LDAP-Backup erfolgreich!" ${EMAILADDRESS} 2>/dev/null
 /usr/bin/logger -is -p local0.info -t ldap_backup LDAP-Backup erfolgreich beendet
